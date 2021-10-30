7db54846efabfb56c616f24868a5f95ebe7a681f
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2021-10-30 21:18:03 +0530

    Initial Commit

---
+++ mapie/mapieApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapieApp.swift
+//  mapie
+//
+//  Created by Prateek on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct mapieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
