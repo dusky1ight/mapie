69198448d933590df0f067d0aaa974baa7e8a6b6
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2021-10-31 13:43:23 +0530

    Added Map layout and location Manager enablement

--- mapie/ContentView.swift
+++ mapie/ContentView.swift
@@ -4,13 +4,23 @@
 //
 //  Created by Prateek on 30/10/21.
 //
-
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331, longitude: -51.0949), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .onAppear{
+                viewModel.checkIfLocationServicesEnabled()
+            }
     }
 }
 
@@ -19,3 +29,52 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+ final class ContentViewModel:NSObject,  ObservableObject, CLLocationManagerDelegate{
+    
+     var locationManager: CLLocationManager?
+    
+     func checkIfLocationServicesEnabled(){
+        if CLLocationManager.locationServicesEnabled(){
+        locationManager = CLLocationManager()
+            locationManager!.delegate = self
+    }
+        else
+        {
+            print("Location Services in turned off!!!")
+        
+        
+        }
+    
+}
+    private func checkLocationAuthorization(){
+         guard let locationManager = locationManager else {
+             return
+         }
+
+         switch locationManager.authorizationStatus{
+             
+        
+         
+         case .notDetermined:
+             locationManager.requestWhenInUseAuthorization()
+         case .restricted:
+             print("Auth. is restricted!!")
+         case .denied:
+             print("Change your authorization settings!!PERMISSION DENIED BY USER")
+         case .authorizedAlways, .authorizedWhenInUse:
+             break
+         @unknown default:
+             break
+         }
+     }
+     
+     
+     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+         checkLocationAuthorization()
+     }
+
+    
+}
+
+
